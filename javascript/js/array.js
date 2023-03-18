d210faf639a0089fc77ada9515346af59ad00cbc
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2023-03-18 19:02:16 +0530

    array methods

---
+++ javascript/js/array.js
@@ -0,0 +1,21 @@
+// array
+// collection more the one data => array
+// array collection of same or different type of
+// declare ==> []
+// array as a set key:value
+// key is also called as index
+// index is auto gen
+// index always starts with 0 i.e zero
+
+// https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v
+// https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices-in.m4v
+
+function a() {}
+var number = [100, 200, 300, 400];
+var array = [10, "edureka", null, true, a, [10, 20, 30]];
+
+console.log(number.length);
+console.log(array);
+
+// can we read single data of array ==> array[index]
+//console.log(array[3]);
