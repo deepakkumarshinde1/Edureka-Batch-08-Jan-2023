07a7f684f31ae302f9ca8410ab9c336d2e5a47ce
deepakkumarshinde3 <deepakkumar.shinde3@gmail.com>
2023-04-29 18:53:37 +0530

    react basic part-1

---
+++ react/my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from "react"; // es6
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("edureka"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
